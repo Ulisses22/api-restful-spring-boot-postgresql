ffa000bfba62b541dde1002ac80e35092e94cf66
Ulisses22 <ulissesnetofreitasalves@gmail.com>
2024-05-04 22:56:46 +0100

    Update ProductModel.java

--- src/main/java/com/example/demo/models/ProductModel.java
+++ src/main/java/com/example/demo/models/ProductModel.java
@@ -1,4 +1,46 @@
 package com.example.demo.models;
 
-public class ProductModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+// Necessário o uso de anotações para criar as entidades no sql
+@Entity
+@Table(name = "TB_PRODUCTS")
+
+public class ProductModel implements Serializable{
+
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
